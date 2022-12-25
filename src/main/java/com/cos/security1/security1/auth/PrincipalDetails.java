847a5738eb30d11cd17e28a75e6ac6a9829f5a2f
고현재 <hjko@gohyeonjaeui-MacBookAir.local>
2022-12-25 14:21:54 +0900

    oauth2

--- src/main/java/com/cos/security1/security1/auth/PrincipalDetails.java
+++ src/main/java/com/cos/security1/security1/auth/PrincipalDetails.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 // 시큐리티가 /login을 낚아채서 로그인을 진행
 // 로그인 진행이 완료가 되면 session을 만들어 준다(security context holder에 키값 저장)
-// 시큐리티 세션의 오브젝트는 정해져 있 => Authentication타입 객체다!
+// 시큐리티 세션의 오브젝트는 정해져 있다 => Authentication타입 객체다!
 // Authentication안에는 User정보가 있어야 함(클래스가 정해져있다)
 // User 오브젝트 타입은 UserDetails 타입 객체
 // Security Session -> Authentication -> UserDetails(principalDetails)
@@ -36,7 +36,7 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
         return attributes;
     }
 
-    // 해당 유저의 권한을 리
+    // 해당 유저의 권한을 리턴
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> collect = new ArrayList<>();
--- src/main/java/com/cos/security1/security1/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/security1/oauth/PrincipalOauth2UserService.java
@@ -2,6 +2,9 @@ package com.cos.security1.security1.oauth;
 
 import com.cos.security1.security1.auth.PrincipalDetails;
 import com.cos.security1.security1.model.User;
+import com.cos.security1.security1.oauth.provider.GoogleUserInfo;
+import com.cos.security1.security1.oauth.provider.NaverUserInfo;
+import com.cos.security1.security1.oauth.provider.Oauth2UserInfo;
 import com.cos.security1.security1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -28,20 +33,27 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     // 함수가 종료되면 @AuthenticationPrincipal 생성
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        // 구글 로그 버튼 -> 구글 로그인 창 -> 로그인 완료 -> code 리턴 -> AccessToken 요청
-        // userReques 정보 -> loadUser 함수로 회원 프로필 얻을 수 있음
+        // 구글 로그 버튼 -> 구글 로그인 창 -> 로그인 완료 -> code 리턴(Oauth - Client 라이브러리) -> AccessToken 요청
+        // userRequest 정보 -> loadUser 함수로 회원 프로필 얻을 수 있음
         // 구글로 부터 userRequest 데이터 받음
         OAuth2User oauth2User = super.loadUser(userRequest);
-        String provider = userRequest.getClientRegistration().getClientId(); // google
-        String providerId = oauth2User.getAttribute("sub");
+        Oauth2UserInfo oauth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            oauth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            oauth2UserInfo = new NaverUserInfo((Map) oauth2User.getAttributes().get("response"));
+        }
+
+        String provider = oauth2UserInfo.getProvider();
+        String providerId = oauth2UserInfo.getProviderId();
         String username = provider + "_" + providerId;
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         String password = bCryptPasswordEncoder.encode("password");
-        String email = oauth2User.getAttribute("email");
+        String email = oauth2UserInfo.getEmail();
         String role ="ROLE_USER";
 
         User userEntity = userRepository.findByUsername(username);
-        if(userEntity == null){
+        if(userEntity == null){ 
             userEntity = User.builder()
                     .username(username)
                     .password(password)
